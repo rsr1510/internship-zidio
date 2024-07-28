4fa66ee129f76099228b8b624d51c9aba7aac6ef
Dhruvi2610 <Dhruvipanchal2610@gmail.com>
2024-07-28 11:54:11 +0530

    mainfolder

---
+++ Frontend/src/Pages/about/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-content">
+        <h2>About Us</h2>
+        <p>
+          Welcome to EduWave!
+
+          At EduWave, we believe in transforming the way you learn. Our platform is dedicated to providing high-quality, accessible, and engaging online education to students and professionals worldwide. With a diverse range of courses, cutting-edge technology, and a community of passionate learners, we aim to make education a wave of endless possibilities.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
